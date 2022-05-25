ffae2c1d454c74080aa910457ebde548e58b4157
root <root@siw-ebvi.sanopool.ch>
2022-05-25 23:51:07 +0200

    Generator verbessert

--- app/db.py
+++ app/db.py
@@ -1,11 +1,13 @@
+import os
 from app import app
 from flaskext.mysql import MySQL
 
 mysql = MySQL()
  
+dbhost = os.environ["DB.HOST"]
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'siwusers'
-app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['MYSQL_DATABASE_HOST'] = dbhost
 mysql.init_app(app)
